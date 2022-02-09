b8b63aacbab51d6ee90ed48211810537459d17c2
Ahmet Meliksah Akdeniz <aahmetmeliksah@gmail.com>
2022-02-09 10:30:39 +0300

    new methods in express | practiced getting  specifik endpoints

--- day3/express-api/server.js
+++ day3/express-api/server.js
@@ -21,17 +21,23 @@ app.get("/posts", (req, res) => {
   }
 });
 /*
-    In short, the if parts of the code above, check if req.qeury (localhost:3000/posts?id=7 - the part after question mark) and if that is true. It creates a variable and filters through posts json file and gets the one that is the exact match for the id. Else, it returns everything.
+    In short, the if parts of the code above, check if req.qeury (localhost:3000/posts?id=7 - the part after question mark) and if that is true. It creates a variable and filters through posts json file and gets the one that is the exact match for the id. Else, it returns everything. Only problem is that users must type something like ?id=8(whatever number) who does that shit? So, the code below is there to solve that problem
 */
 
-app.get("/users", (req, res) => {
-  if (req.query.id) {
-    let filteredUsers = users.filter((x) => x.id == req.query.id);
-    res.send(filteredUsers);
-  } else {
-    res.send(users);
-  }
+app.get("/users/:userId", (req, res) => {
+  let fileteredUser = users.filter((x) => x.id == req.params.userId);
+  res.send(fileteredUser);
 });
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+/*
+    Explaining two code blocks above. First of all this is how you write your code if you want the user to get rid of this posts?id=7 weird route. In your get() method put "/users/:usersId" first. Obivously it does not have to be users... Should I even talk about that??? Use req.params.userId(or whatever you put as route) and bring back only the one that matches the search. 
+
+    When /users is requested get it on another route because if else statement does not work for
+     /users/:userId. In other words, define params endpoints and normal endpoints seperately. 
+*/
 
 const PORT = 3000;
 app.listen(PORT, () => {
