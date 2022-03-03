69e5d4d79e1a99492a8a832c0c5a67a2609f57a6
Ahmet Meliksah Akdeniz <71570633+aahmetmeliksah@users.noreply.github.com>
2022-03-03 17:12:31 +0300

    delete

--- day5/rest_server/src/controllers/post_controller.js
+++
@@ -1,9 +0,0 @@
-const postData = require("../data/posts_data_access");
-
-const getAllPostsControlller = async () => {
-  let allPosts = await postData.getAllPosts();
-
-  return allPosts;
-};
-
-module.exports = { getAllPostsControlller };
--- day5/rest_server/src/data/posts_data_access.js
+++
@@ -1,7 +0,0 @@
-const axios = require("axios");
-
-const getAllPosts = () => {
-  return axios.get("https://jsonplaceholder.typicode.com/posts");
-};
-
-module.exports = { getAllPosts };
