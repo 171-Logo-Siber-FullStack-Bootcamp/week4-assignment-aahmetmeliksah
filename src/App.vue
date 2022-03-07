2940f24ae94cd72099052125d7ce03e50eff6ae8
Ahmet Meliksah Akdeniz <aahmetmeliksah@gmail.com>
2022-03-07 20:04:42 +0300

    bug fixed

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,62 @@
 <template>
-  <div>
-    <div id="page-top">
-      <WrapperComponentIndex />
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+    <!-- Sidebar -->
+    <ul
+      class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion"
+      id="accordionSidebar"
+    >
+      <SideBrand />
+      <SideMenu />
+    </ul>
+    <!-- End of Sidebar -->
 
-      <ScrollComponent />
-
-      <LogoutComponent />
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+      <!-- Main Content -->
+      <div id="content">
+        <!-- Topbar -->
+        <nav
+          class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow"
+        >
+          <TopSearch />
+          <TopNav />
+        </nav>
+        <!-- End of Topbar -->
+        <ContentComp />
+      </div>
+      <!-- End of Main Content -->
+      <!-- Footer -->
+      <FooterComp />
+      <!-- End of Footer -->
     </div>
   </div>
+  <LogoutComp />
+  <ScrollTop />
 </template>
 
 <script>
-import ScrollComponent from "./components/ScrollComp.vue";
-import WrapperComponentIndex from "./components/ContentWrapper/WrapperComponentIndex.vue";
-import LogoutComponent from "./Components/LogoutComponent.vue";
+// import SideBar from './components/SideBar'
+// import TopBar from './components/TopBar'
+import FooterComp from "./components/FooterComp.vue";
+import ContentComp from "./components/ContentComp.vue";
+import LogoutComp from "./components/LogOutComp.vue";
+import ScrollTop from "./components/ScrollTop.vue";
+import SideBrand from "./components/topAndSideBar/SideBarCmp/SideBrand.vue";
+import SideMenu from "./components/topAndSideBar/SideBarCmp/SideMenu.vue";
+import TopSearch from "./components/topAndSideBar/TopBarCmp/TopSearch.vue";
+import TopNav from "./components/topAndSideBar/TopBarCmp/TopNav.vue";
 export default {
+  name: "App",
   components: {
-    ScrollComponent,
-    WrapperComponentIndex,
-    LogoutComponent,
+    FooterComp,
+    ContentComp,
+    LogoutComp,
+    ScrollTop,
+    SideBrand,
+    SideMenu,
+    TopSearch,
+    TopNav,
   },
 };
 </script>
